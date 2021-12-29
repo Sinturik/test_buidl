3129f344740e2148b99f99208126acc1465ea287
sin <sinturik@gmail.com>
2021-12-30 00:11:09 +0600

    added some tests

---
+++ src/test/java/NegativeTests.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test0() {
+        assertFalse(true);
+    }
+
+    @Test
+    void test1() {
+        assertFalse(true);
+    }
+
+    @Test
+    void test2() {
+        assertFalse(true);
+    }
+
+    @Test
+    void test3() {
+        assertFalse(true);
+    }
+
+    @Test
+    void test4() {
+        assertFalse(true);
+    }
+}
---
+++ src/test/java/SkippedTests.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SkippedTests {
+
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("Some reason")
+    void test2() {
+        assertTrue(true);
+    }
+
+
+}
